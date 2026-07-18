24b0ba87f78b5900372bc47ae62b7a1346efab6f
tgrmayank <ivipjaat001@gmail.com>
2026-07-19 00:17:48 +0530

    Update Info.js

--- Config/Info.js
+++ Config/Info.js
@@ -1,9 +1,7 @@
 module.exports = {
-
-owner:"👑 OWNER MAYANK",
-tg:"📩 TG @IMAYANKFF",
-group:"👥 TG GROUP @TGRMAYANK",
-api:"⚡ API NAME",
-buy:"💎 API BUY TO DM"
-
+  owner: "👑 OWNER MAYANK",
+  tg: "📩 TG @IMAYANKFF",
+  group: "👥 TG GROUP @TGRMAYANK",
+  api: "⚡ TGR PINCODE API",
+  buy: "💎 API BUY TO DM"
 };
